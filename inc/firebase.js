2cf09c3998f97037935d0d766e75872219ef8970
seanpierce <contact@seanpierce.us>
2017-09-30 12:31:18 -0700

    add firebase keys

---
+++ inc/firebase.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAhUHPwura_xI3Gl7Qi5QNlor8igP7VWIk",
+  authDomain: "boogie-bu.firebaseapp.com",
+  databaseURL: "https://boogie-bu.firebaseio.com",
+  projectId: "boogie-bu",
+  storageBucket: "",
+  messagingSenderId: "636626897207"
+};
+firebase.initializeApp(config);

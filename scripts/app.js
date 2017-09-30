fc16db16dead494a0e01bf00fb6878fff7d33760
seanpierce <contact@seanpierce.us>
2017-09-30 13:48:32 -0700

    call display_events to print to page

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,12 @@
 $(function() {
-  console.log(events);
+  ref.on("value", function(snapshot) {
+    // erase all prev events
+    $('.event').remove();
+    snapshot.forEach(function(event) {
+      // print events to page
+      $('#events').append(display_event(event));
+    });
+  }, function (error) {
+    console.log("Error: " + error.code);
+  });
 });

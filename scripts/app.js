63dafa82388db957a0abd632ca1ae43981a7f6cd
seanpierce <contact@seanpierce.us>
2017-09-30 13:49:47 -0700

    move business logic from database js to app

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,18 @@
+// display event to #event div
+let display_event = function(event) {
+  let key = event.key;
+  let this_event = event.val();
+  let html = "<p>hello</p>"
+
+  console.log(this_event);
+  return html;
+}
+
+
+// ------------------------------ document ready
+// ------------------------------ document ready
+// ------------------------------ document ready
+
 $(function() {
   ref.on("value", function(snapshot) {
     // erase all prev events
